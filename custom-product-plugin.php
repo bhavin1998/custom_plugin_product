a21feff08f6b43aaa3764a5ee8e38a2910db756a
bhavin1998 <bhavingediya123@gmail.com>
2022-05-27 18:53:35 +0530

    Upload meta box for image

--- custom-product-plugin.php
+++ custom-product-plugin.php
@@ -79,3 +79,84 @@ function crunchify_create_deals_custom_product_taxonomy() {
       'rewrite' => array( 'slug' => 'type' ),
     ));
 }
+
+function misha_include_myuploadscript() {
+    /*
+     * I recommend to add additional conditions just to not to load the scipts on each page
+     * like:
+     * if ( !in_array('post-new.php','post.php') ) return;
+     */
+    if ( ! did_action( 'wp_enqueue_media' ) ) {
+        wp_enqueue_media();
+    }
+
+    wp_enqueue_script( 'myuploadscript',  plugin_dir_url(__FILE__).'js/customjs.js', array('jquery'), null, false );
+}
+
+add_action( 'admin_enqueue_scripts', 'misha_include_myuploadscript' );
+
+function misha_image_uploader_field( $name, $value = '') {
+    $image = ' button">Upload image';
+    $image_size = 'full'; // it would be better to use thumbnail size here (150x150 or so)
+    $display = 'none'; // display state ot the "Remove image" button
+
+    if( $image_attributes = wp_get_attachment_image_src( $value, $image_size ) ) {
+
+        // $image_attributes[0] - image URL
+        // $image_attributes[1] - image width
+        // $image_attributes[2] - image height
+
+        $image = '"><img src="' . $image_attributes[0] . '" style="max-width:95%;display:block;" />';
+        $display = 'inline-block';
+
+    } 
+
+    return '
+    <div>
+        <a href="#" class="misha_upload_image_button' . $image . '</a>
+        <input type="hidden" name="' . $name . '" id="' . $name . '" value="' . $value . '" />
+        <a href="#" class="misha_remove_image_button" style="display:inline-block;display:' . $display . '">Remove image</a>
+    </div>';
+}
+
+/*
+ * Add a meta box
+ */
+add_action( 'admin_menu', 'misha_meta_box_add' );
+
+function misha_meta_box_add() {
+    add_meta_box('mishadiv', // meta box ID
+        'More settings', // meta box title
+        'misha_print_box', // callback function that prints the meta box HTML 
+        'my-product', // post type where to add it
+        'normal', // priority
+        'high' ); // position
+}
+
+/*
+ * Meta Box HTML
+ */
+function misha_print_box( $post ) {
+    $meta_key = 'second_featured_img';
+    echo misha_image_uploader_field( $meta_key, get_post_meta($post->ID, $meta_key, true) );
+}
+
+/*
+ * Save Meta Box data
+ */
+add_action('save_post', 'misha_save');
+
+function misha_save( $post_id ) {
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+        return $post_id;
+
+    $meta_key = 'second_featured_img';
+    $_POST[$meta_key] = '';
+
+    update_post_meta( $post_id, $meta_key, $_POST[$meta_key] );
+
+    // if you would like to attach the uploaded image to this post, uncomment the line:
+    // wp_update_post( array( 'ID' => $_POST[$meta_key], 'post_parent' => $post_id ) );
+
+    return $post_id;
+}
